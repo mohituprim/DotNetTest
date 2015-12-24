3aff797714410f7a1866fe84fe03c2821d098441
mohit kumar <mohituprim@gmail.com>
2015-12-24 12:00:57 +0530

    mohit first test

---
+++ MvcApplicationTest/MvcApplicationTest/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplicationTest.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string GetString()
+        {
+            return "Hello World is old now. It&rsquo;s time for wassup bro ;)";
+        }
+    }
+}
